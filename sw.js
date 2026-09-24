bcc73f4c1ac0df55968be4890f07510eb4f5c2ad
denverufb <aereon@fomusic.org>
2026-09-24 13:21:02 -0500

    Add mobile clipboard paste for Script URL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v7";
+const CACHE = "fnh-field-check-github-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
