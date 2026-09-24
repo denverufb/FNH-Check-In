3907f42e99009de9f334111253da747ff42c7df5
denverufb <aereon@fomusic.org>
2026-09-24 13:52:42 -0500

    Remove clock delay from live teacher updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v10";
+const CACHE = "fnh-field-check-github-v11";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
