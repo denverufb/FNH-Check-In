bf8c435cc69ed87462592a014c92c5f474c733f2
denverufb <aereon@fomusic.org>
2026-09-24 17:26:10 -0500

    Keep student timelines open during live refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v19";
+const CACHE = "fnh-field-check-github-v20";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
