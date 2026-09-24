b7c1de126ee0c0d46fd356c6f66a4bbc63f914fd
denverufb <aereon@fomusic.org>
2026-09-24 17:31:48 -0500

    Add distinct teacher status alert sounds

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v20";
+const CACHE = "fnh-field-check-github-v21";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
