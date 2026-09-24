e372cb6d559b03875691cf41b59b18b4094117b6
denverufb <aereon@fomusic.org>
2026-09-24 13:42:29 -0500

    Make every student step sync and refresh teacher view continuously

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v9";
+const CACHE = "fnh-field-check-github-v10";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
