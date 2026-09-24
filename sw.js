40b4d85906dbbef0fab65ab2828a9810e1e5d6b5
denverufb <aereon@fomusic.org>
2026-09-24 16:43:57 -0500

    Keep approved roster visible with older Sheet data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v14";
+const CACHE = "fnh-field-check-github-v15";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
