8f82214863fb3074cb237e9aa4aff8dd0bedffe9
denverufb <aereon@fomusic.org>
2026-09-24 18:36:57 -0500

    Redesign student controls as circular buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v26";
+const CACHE = "fnh-field-check-github-v27";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
