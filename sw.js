f0c66eda91aa56be80197a3d8fb0fd45f77dcba6
denverufb <aereon@fomusic.org>
2026-09-24 17:12:56 -0500

    Play a chime for new teacher messages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v17";
+const CACHE = "fnh-field-check-github-v18";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
