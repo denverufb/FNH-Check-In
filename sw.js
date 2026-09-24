9c7b1f7af342f4ce9d403ed85ecc58cbd029e920
denverufb <aereon@fomusic.org>
2026-09-24 18:04:15 -0500

    Keep teacher map free to pan during refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v23";
+const CACHE = "fnh-field-check-github-v24";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
