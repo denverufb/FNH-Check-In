f4d2dfadbbccd0e6ae53f1128a54e79f297938d3
denverufb <aereon@fomusic.org>
2026-09-25 07:45:55 -0500

    Prevent shared log polling overload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v28";
+const CACHE = "fnh-field-check-github-v29";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
