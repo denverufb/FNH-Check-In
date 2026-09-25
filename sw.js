59ad967c7eb201d8254fa79a689e9057b76a3a06
denverufb <aereon@fomusic.org>
2026-09-25 08:06:10 -0500

    Crop student portraits without color frames

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v30";
+const CACHE = "fnh-field-check-github-v31";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
