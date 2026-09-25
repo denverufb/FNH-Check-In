368a278b1513dbea3f60803fb620d68589e3a9da
denverufb <aereon@fomusic.org>
2026-09-25 07:15:41 -0500

    Reset student apps at end of day

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v27";
+const CACHE = "fnh-field-check-github-v28";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
