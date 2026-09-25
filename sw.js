1ddf06cfd0e17ed4ed8cb654a14fd0e6dc6de433
denverufb <aereon@fomusic.org>
2026-09-25 08:09:09 -0500

    Restore messaging and preserve permission setup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fnh-field-check-github-v31";
+const CACHE = "fnh-field-check-github-v32";
 const ASSETS = ["./", "./index.html", "./teacher.html", "./manifest.webmanifest", "./icon.svg", "./assets/student-portraits.png"];
 
 self.addEventListener("install", (event) => {
